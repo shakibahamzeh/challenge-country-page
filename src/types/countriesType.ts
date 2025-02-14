5a53c6e57bc0ad1a7df96d6d1afa4b69e5e89ee5
shakiba Hamzeh <shakiba.h_70@yahoo.com>
2025-02-14 22:23:43 +0330

    add types of countryRanking component

---
+++ src/types/countriesType.ts
@@ -0,0 +1,28 @@
+// types/countriesType.ts
+export interface Header {
+  id: number;
+  title: string;
+}
+
+export interface Country {
+  flags: { svg: string };
+  name: { common: string };
+  population: number;
+  region: string;
+  subregion: string;
+  unMember: boolean;
+  independent: boolean;
+  area: number;
+}
+
+export interface TableProps {
+  countries: Country[];
+  isPending: boolean;
+  page: number;
+  headers: Header[];
+}
+
+export interface SearchInputProps {
+  search: string;
+  setSearch: (value: string) => void;
+}

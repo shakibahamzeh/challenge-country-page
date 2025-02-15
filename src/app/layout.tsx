6c49aa6454241be638b61b9f89382af330bf23dc
shakiba Hamzeh <shakiba.h_70@yahoo.com>
2025-02-15 21:27:53 +0330

    add font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import ReactQueryProvider from "../provider/ReactQueryProvider";
+import { Be_Vietnam_Pro} from "next/font/google"
 import "./globals.css";
+
+const vietnam = Be_Vietnam_Pro({ 
+  subsets: ["latin"], 
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-vietnam"
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={vietnam.variable}>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>

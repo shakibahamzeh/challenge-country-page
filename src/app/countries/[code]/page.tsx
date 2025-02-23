f928cab19b26243691c1605b26e59278f6883964
Shakiba Hamzeh <S.Hamzeh@nitro.local>
2025-02-23 16:31:00 +0330

    change layout

--- src/app/countries/[code]/page.tsx
+++ src/app/countries/[code]/page.tsx
@@ -17,11 +17,11 @@ export default function CountryPage() {
   if (!country) return <p>Country not found</p>;
 
   return (
-    <div className="grid grid-cols-[1fr_1fr] gap-2 p-16">
-      <div>
-       <Image src={country[0].flags?.svg} alt="flag" width={500} height={500} className="rounded" />
+    <div className="bg-outerBackground absolute top-[23vh] left-0 right-0 p-3 sm:p-5 3xl:mx-[300px] 2xl:mx-[250px] xl:mx-[200px] lg:mx-[150px] md:mx-[100px] sm:mx-[50px] mx-0 sm:rounded-2xl flex flex-col gap-5">
+      <div className="absolute top-[-70px] left-[50%] translate-x-[-50%]">
+       <Image src={country[0].flags?.svg} alt="flag" width={400} height={500} className="rounded" />
       </div>
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3 ">
         <h1 className="text-3xl font-bold">{country[0].name?.common}</h1>
         <p className="text-lg">Capital: {country[0].capital[0]}</p>
         <p className="text-lg">Region: {country[0].region}</p>

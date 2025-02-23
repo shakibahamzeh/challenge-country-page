0fec6fe682cde33685c0f13a12ba971730034f73
Shakiba Hamzeh <S.Hamzeh@nitro.local>
2025-02-23 16:30:46 +0330

    change layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import ReactQueryProvider from "../provider/ReactQueryProvider";
 import { Be_Vietnam_Pro} from "next/font/google"
 import "./globals.css";
+import Image from "next/image";
 
 const vietnam = Be_Vietnam_Pro({ 
   subsets: ["latin"], 
@@ -12,7 +13,38 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={vietnam.variable}>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <ReactQueryProvider>
+          <div className="relative min-h-screen">
+            <div className="relative w-full h-[35vh]">
+              <div className="absolute left-0 right-0 top-[110px] flex justify-center">
+                <Image 
+                  src="/Logo.svg"
+                  alt="logo"
+                  width={200}
+                  height={25}
+                  priority
+                />
+              </div>
+              <Image
+                src="/hero-image.jpg"
+                alt="Hero Image"
+                width={1920}
+                height={600}
+                className="w-full h-full object-cover hidden sm:block"
+                priority
+              />
+              <Image
+                src="/hero-image-sm.jpg"
+                alt="Hero Image"
+                width={640}
+                height={300}
+                className="w-full h-full object-cover sm:hidden"
+                priority
+              />
+            </div>
+            {children}
+          </div>          
+        </ReactQueryProvider>
       </body>
     </html>
   );
